08f84f86e838c384f3100ffb96d43bfdc031a858
Guillaume Rose <gurose@redhat.com>
2021-09-30 12:03:43 +0200

    Configure the stack to handle IPv6 packets

--- cmd/gvproxy/config.go
+++ cmd/gvproxy/config.go
@@ -204,6 +204,9 @@ func GvproxyConfigure(config *GvproxyConfig, args *GvproxyArgs, version string) 
 	if config.Stack.GatewayMacAddress == "" {
 		config.Stack.GatewayMacAddress = "5a:94:ef:e4:0c:dd"
 	}
+	if config.Stack.GatewayIPv6 == "" {
+		config.Stack.GatewayIPv6 = "fd00::1"
+	}
 	if len(config.Stack.NAT) == 0 {
 		config.Stack.NAT = map[string]string{
 			luaddr.String(): "127.0.0.1",
