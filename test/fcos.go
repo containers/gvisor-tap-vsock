33ad29de8716f0d1d30329a58a7ce91a1df2f86c
Christophe Fergeau <cfergeau@redhat.com>
2024-05-28 13:47:26 +0200

    test: Make getFCOSDownload less arch-dependent
    It now supports amd64, arm64, ppc64le and s390x, which are the 4 arches
    for which there are fcos images.
    
    Signed-off-by: Christophe Fergeau <cfergeau@redhat.com>

--- test/fcos.go
+++ test/fcos.go
@@ -5,7 +5,17 @@ import (
 	"path/filepath"
 	"strings"
 
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"runtime"
+
 	"github.com/opencontainers/go-digest"
+
+	"github.com/coreos/stream-metadata-go/fedoracoreos"
+	"github.com/coreos/stream-metadata-go/stream"
+	"github.com/sirupsen/logrus"
 )
 
 type FcosDownload struct {
@@ -77,4 +87,72 @@ func (downloader *FcosDownload) updateAvailable(info *fcosDownloadInfo, compress
 		return true, nil
 	}
 	return false, nil
+}
+
+// as of 2024-05-28, these are the 4 architectures available in
+// curl https://builds.coreos.fedoraproject.org/streams/next.json
+func coreosArch() string {
+	switch runtime.GOARCH {
+	case "amd64":
+		return "x86_64"
+	case "arm64":
+		return "aarch64"
+	case "ppc64le":
+		return "ppc64le"
+	case "s390x":
+		return "s390x"
+	}
+	panic(fmt.Sprintf("unknown arch: %s", runtime.GOOS))
+}
+
+// This should get Exported and stay put as it will apply to all fcos downloads
+// getFCOS parses fedoraCoreOS's stream and returns the image download URL and the release version
+func getFCOSDownload() (*fcosDownloadInfo, error) {
+	streamurl := fedoracoreos.GetStreamURL(fedoracoreos.StreamNext)
+	resp, err := http.Get(streamurl.String())
+	if err != nil {
+		return nil, err
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			logrus.Error(err)
+		}
+	}()
+
+	var fcosstable stream.Stream
+	if err := json.Unmarshal(body, &fcosstable); err != nil {
+		return nil, err
+	}
+	arch, ok := fcosstable.Architectures[coreosArch()]
+	if !ok {
+		return nil, fmt.Errorf("unable to pull VM image: no targetArch in stream")
+	}
+	artifacts := arch.Artifacts
+	if artifacts == nil {
+		return nil, fmt.Errorf("unable to pull VM image: no artifact in stream")
+	}
+	qemu, ok := artifacts["qemu"]
+	if !ok {
+		return nil, fmt.Errorf("unable to pull VM image: no qemu artifact in stream")
+	}
+	formats := qemu.Formats
+	if formats == nil {
+		return nil, fmt.Errorf("unable to pull VM image: no formats in stream")
+	}
+	qcow, ok := formats["qcow2.xz"]
+	if !ok {
+		return nil, fmt.Errorf("unable to pull VM image: no qcow2.xz format in stream")
+	}
+	disk := qcow.Disk
+	if disk == nil {
+		return nil, fmt.Errorf("unable to pull VM image: no disk in stream")
+	}
+	return &fcosDownloadInfo{
+		Location:  disk.Location,
+		Sha256Sum: disk.Sha256,
+	}, nil
 }
