911a7514b0a91d84654ed6ef38adc05d9d9e076f
Gunjan Vyas <vyasgun20@gmail.com>
2025-08-01 16:26:33 +0530

    Replace VFKT handshake with MSG_PEEK for vfkit connection identification
    Signed-off-by: Gunjan Vyas <vyasgun20@gmail.com>

--- pkg/transport/unixgram_darwin.go
+++ pkg/transport/unixgram_darwin.go
@@ -4,9 +4,8 @@
 package transport
 
 import (
-	"bytes"
-	"encoding/hex"
 	"fmt"
+
 	"net"
 	"syscall"
 )
@@ -47,21 +46,68 @@ func (conn *connectedUnixgramConn) Write(b []byte) (int, error) {
 	return conn.WriteTo(b, conn.remoteAddr)
 }
 
-func AcceptVfkit(listeningConn *net.UnixConn) (net.Conn, error) {
-	vfkitMagic := make([]byte, 4)
-	// the main reason for this magic check is to get the address to use to send data to the vfkit VM
-	bytesRead, vfkitAddr, err := listeningConn.ReadFrom(vfkitMagic)
-	if bytesRead != len(vfkitMagic) {
-		return nil, fmt.Errorf("invalid magic length: %d", len(vfkitMagic))
-	}
+func peekAddress(listeningConn *net.UnixConn) (*syscall.SockaddrUnix, error) {
+	rawConn, err := listeningConn.SyscallConn()
 	if err != nil {
 		return nil, err
 	}
-	if _, ok := vfkitAddr.(*net.UnixAddr); !ok {
-		return nil, fmt.Errorf("unexpected type for vfkit unix sockaddr: %t", vfkitAddr)
+
+	var vfkitSockaddr syscall.Sockaddr
+	var getRemoteAddrErr error
+
+	magic := make([]byte, 4)
+	getRemoteAddr := func(fd uintptr) bool {
+		_, vfkitSockaddr, getRemoteAddrErr = syscall.Recvfrom(int(fd), magic, syscall.MSG_PEEK|syscall.MSG_TRUNC)
+		return true
 	}
-	if !bytes.Equal(vfkitMagic, []byte("VFKT")) {
-		return nil, fmt.Errorf("invalid magic from the vfkit process: %s", hex.EncodeToString(vfkitMagic))
+	if err := rawConn.Read(getRemoteAddr); err != nil {
+		return nil, err
 	}
-	return connectListeningUnixgramConn(listeningConn, vfkitAddr.(*net.UnixAddr))
+	if getRemoteAddrErr != nil {
+		return nil, getRemoteAddrErr
+	}
+
+	// If it's the old vfkit handshake, consume it
+	if string(magic) == "VFKT" {
+		_, _, err = listeningConn.ReadFrom(magic)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	vfkitSockaddrUnix, ok := vfkitSockaddr.(*syscall.SockaddrUnix)
+	if !ok {
+		return nil, fmt.Errorf("unexpected remote address type: %t", vfkitSockaddr)
+	}
+	return vfkitSockaddrUnix, nil
+}
+
+func AcceptVfkit(listeningConn *net.UnixConn) (net.Conn, error) {
+	file, err := listeningConn.File()
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	fd := int(file.Fd())
+	readFds := &syscall.FdSet{}
+	readFds.Bits[fd/64] |= 1 << (uint(fd) % 64)
+
+	// Select() blocks until the socket is readable
+	err = syscall.Select(fd+1, readFds, nil, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	peekedAddr, err := peekAddress(listeningConn)
+	if err != nil {
+		return nil, err
+	}
+
+	if peekedAddr.Name == "" {
+		return nil, fmt.Errorf("no address found")
+	}
+
+	vfkitAddr := &net.UnixAddr{Name: peekedAddr.Name, Net: "unixgram"}
+	return connectListeningUnixgramConn(listeningConn, vfkitAddr)
 }
